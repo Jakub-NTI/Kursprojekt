ad776d762d9d024a8bf94abb6292eff5f4b33393
Jakub-NTI <jakub.zabramski@elev.ga.ntig.se>
2023-05-17 15:52:22 +0200

    Me balls
    hurt

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-/*sidebar*/
-// Open or Close Sidebar
+//Sidebar
+let sections = document.querySelector(".sections")
+let sidebar = document.querySelector(".sidebar")
+let closeSidebar = document.querySelector(".close-sidebar")
+
+// open or close sidebar
 sections.onclick = () => {
     sidebar.classList.add("active")
 };
-sections.onclick = () => {
+closeSidebar.onclick = () => {
     sidebar.classList.remove("active")
 };
+
+// sidebar working js
+if (document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", ready);
+} else {
+    ready();
+}
+
